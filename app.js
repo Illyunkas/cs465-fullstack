2d4fbe0f80692ba9267b305b301e87da838eb184
Dan Peterson <dan.peterson@snhu.edu>
2026-07-09 13:17:18 -0700

    MVC architecture and travel route implemented

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+
+// added variable required "travelRouter"
+var travelRouter = require('./app_server/routes/travel'); 
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// add app uses
+app.use('/travel', travelRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
