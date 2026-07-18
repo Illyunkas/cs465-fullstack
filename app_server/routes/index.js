77c3d0ba06d341eecc740f7caee3a3b3f8550157
Dan Peterson <dan.peterson@snhu.edu>
2026-07-18 10:17:16 -0700

    Implement meals and rooms routes with controller exports and views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,20 @@
+
+
 var express = require('express');
 var router = express.Router();
 
+// Import your controllers (you will need to create/have these files)
+var ctrlMain = require('../controllers/travel');
+var ctrlRooms = require('../controllers/rooms');
+var ctrlMeals = require('../controllers/meals');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.travel);
+
+/* GET rooms page. */
+router.get('/rooms', ctrlRooms.rooms);
+
+/* GET meals page. */
+router.get('/meals', ctrlMeals.meals);
 
 module.exports = router;
